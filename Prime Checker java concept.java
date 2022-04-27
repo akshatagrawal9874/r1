685695b1a7fb8eca6fc9a3db76c0ca08ab6eab81
Akshat Agrawal <akshatagrawal9874@gmail.com>
2022-04-27 13:05:17 +0530

    Create Prime Checker java concept.java

---
+++ Prime Checker java concept.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+import static java.lang.System.in;
+class Prime
+{
+    public static void checkPrime(int ...a)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            boolean f=true;
+            for(int j=2;j*j<=a[i];j++)
+            {
+                if(a[i]%j==0)
+                {
+                    f=false;
+                    break;
+                }
+            }
+            if(f && a[i]!=1)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        System.out.println();
+    }
+}
+public class Solution {
+
+	public static void main(String[] args) {
+		try{
+		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		int n1=Integer.parseInt(br.readLine());
+		int n2=Integer.parseInt(br.readLine());
+		int n3=Integer.parseInt(br.readLine());
+		int n4=Integer.parseInt(br.readLine());
+		int n5=Integer.parseInt(br.readLine());
+		Prime ob=new Prime();
+		ob.checkPrime(n1);
+		ob.checkPrime(n1,n2);
+		ob.checkPrime(n1,n2,n3);
+		ob.checkPrime(n1,n2,n3,n4,n5);	
+		Method[] methods=Prime.class.getDeclaredMethods();
+		Set<String> set=new HashSet<>();
+		boolean overload=false;
+		for(int i=0;i<methods.length;i++)
+		{
+			if(set.contains(methods[i].getName()))
+			{
+				overload=true;
+				break;
+			}
+			set.add(methods[i].getName());
+			
+		}
+		if(overload)
+		{
+			throw new Exception("Overloading not allowed");
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+}
+
